eaeb3900d883d456f9ac678999b6587b588b405c
santukumar01 <santu13102000@gmail.com>
2024-04-17 10:10:46 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:5173"  // forontend server
+        origin: "*"  // forontend server
     }
 
 })
