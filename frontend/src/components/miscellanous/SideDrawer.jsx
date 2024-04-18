687c8208c05c55db134850c718c36d2d6ea9db7b
santu kumar <89204695+santukumar01@users.noreply.github.com>
2024-04-18 18:46:08 +0530

    Update SideDrawer.jsx

--- frontend/src/components/miscellanous/SideDrawer.jsx
+++ frontend/src/components/miscellanous/SideDrawer.jsx
@@ -138,7 +138,7 @@ const SideDrawer = () => {
                     </Button>
                 </Tooltip>
                 <Text fontSize="2xl" fontFamily="Work sans">
-                    Lets - Connect- Again
+                    Let's - Connect
                 </Text>
                 <div>
 
