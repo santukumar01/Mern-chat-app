10fe109363de5bf8a55ea4e40bd9fe1a7be1a24e
santu kumar <89204695+santukumar01@users.noreply.github.com>
2024-05-30 12:12:47 +0530

    Update HomePage.jsx

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -38,7 +38,7 @@ function Homepage() {
                 borderWidth="1px"
             >
                 <Text fontSize="4xl" fontFamily="Work sans">
-                    Talk-A-Tive
+                    Let's - connect 
                 </Text>
             </Box>
             <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
